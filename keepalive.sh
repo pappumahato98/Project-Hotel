c34931ff8671ba3e5b87251ed22f2d51209e5e4a
Z User <z@container>
2026-06-05 04:20:40 +0000

    2c71e82a-87ad-441c-992d-2033a28b21f0

---
+++ keepalive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/z/my-project
+while true; do
+    npx next dev -p 3000 -H 0.0.0.0 2>&1 | tee /home/z/my-project/dev.log
+    echo "Server exited, restarting in 3s..."
+    sleep 3
+    rm -rf .next
+done
